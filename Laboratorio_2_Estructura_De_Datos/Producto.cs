6c6e01ab87ce1541f5efdc82ca6f6ce1e42fd6a6
David <davideliasromeroclaros@gmail.com>
2024-09-20 22:55:40 -0600

    Ingreso de nuevos productos

--- Laboratorio_2_Estructura_De_Datos/Producto.cs
+++ Laboratorio_2_Estructura_De_Datos/Producto.cs
@@ -8,19 +8,50 @@ namespace Laboratorio_2_Estructura_De_Datos
 {
     internal class Producto
     {
+        //El id se generara al momento de crear una instancia de esta clase y
+        //corresponde con el indice de almacenamiento en el diccionario donde se almacenan
+        //los productos
+        public int Id { get; set; }
 
-        int Id { get; set; } //autogenerada
-        string Nombre { get; set; }
-        double Precio { get; set; }
-        int cantidad { get; set; }
-        string fecha { get; set; }//auto generada
+        public string Nombre { get; set; }
 
-        public Producto(string nombre, double precio, int cantidad)
+        //Se actualiza con el precio unitario de cada producto del lote mas reciente
+        public double Precio { get; set; } 
+        
+        //Se calcula a partir de la cantidad que hay en cada lote
+        public int Cantidad { get; set; }
+        
+        //Todo producto que entra se tratara como un lote y se almacenara aqui
+        public static List<Lote> Lotes { get; set; } = new List<Lote>();
+
+        //Este constructor se utilizara la primera que se agregue producto
+        //el producto no existe
+        public Producto(string nombre, double precio, int id, Lote lote) 
         {
-            //el id se generara al momento de crear las instancias de cada producto
-            Nombre = nombre;
-            Precio = precio;
-            this.cantidad = cantidad;
+            this.Id = id;
+
+            this.Nombre = nombre;
+
+            this.Precio = precio;
+
+            Lotes.Add(lote);
+
+            //Actualizar cantidad disponible de producto 
+            //sumamos la cantidad en cada lote
+            this.Cantidad = this.Cantidad + lote.Cantidad;
+        }
+
+        //Se utilizara cuando solo queremos agregar mas producto
+        //el producto ya existe, es decir, solo queremos agregar un nuevo lote
+        public void agregarLote(double precio,Lote lote)
+        {
+            this.Precio = precio;
+
+            Lotes.Add(lote);
+
+            //Actualizar cantidad disponible de producto 
+            //sumamos la cantidad en cada lote
+            this.Cantidad = this.Cantidad + lote.Cantidad;
         }
     }
 }
--- Laboratorio_2_Estructura_De_Datos/Program.cs
+++ Laboratorio_2_Estructura_De_Datos/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Laboratorio_2_Estructura_De_Datos
@@ -11,84 +12,85 @@ namespace Laboratorio_2_Estructura_De_Datos
     {
         static void Main(string[] args)
         {
-            // Aqui se almacenan los productos
-            Dictionary<int, List<Producto>> inventario = new Dictionary<int, List<Producto>>();
-
-            // Nombre de la empresa
+            //Nombre de la empresa
             string nombreEmpresa;
             Console.Write("Ingrese el nombre de la empresa: ");
             nombreEmpresa = Console.ReadLine();
 
-
             while (true)
             {
-                Console.Clear(); // limpiar consola
+                Console.Clear(); //Limpiar consola
+                Console.WriteLine("***********************************************************");
                 Console.WriteLine("GESTION DE INVENTARIO EMPRESA: " + nombreEmpresa);
+                Console.WriteLine("***********************************************************");
 
+
+                //Menu principal 
                 Console.WriteLine("\nMenu principal ...");
-                Console.WriteLine("\nIngrese una opcion: ");
-                int opMenu;
-                //menu de modulos --> cada modulo tiene su propio submenu
-                //MENU PRINCIPAL--------
+                int opMenu; //Esta misma variable se reutiliza en todos los menus
                 Console.WriteLine("1.Agregar productos a inventario");
                 Console.WriteLine("2.Vender productos del inventario");
                 Console.WriteLine("3.Consultar inventario");
                 Console.WriteLine("4.Salir");
                 Console.Write("\nOpcion: ");
-
+                // Validacion de opcion ingresada
                 while (!int.TryParse(Console.ReadLine(), out opMenu) || opMenu < 1 || opMenu > 4)
                 {
                     Console.WriteLine("Opcion no valida");
+                    Console.Write("\nOpcion: ");
                 }
 
-
-                if (opMenu == 4)//ESTE IF SIRVE PARA CONTROLAR EL BUCLE (SALIR)
+                //Para salir del menu principal (bucle while) el programa finaliza
+                if (opMenu == 4)
                 {
                     break;
                 }
 
-
-                Console.Clear();
+                Console.Clear(); //Limpiar consola
                 switch (opMenu)
                 {
                     case 1:
-                        //este case para manejar el ingreso de productos ya sea peps o ueps , costo promedio
-                        //permitir que el user seleccione que metodo usar
+                        //Este case es para manejar el ingreso de productos ya sea peps, ueps o costo promedio
+                        //El usuario selecciona el metodo de gestion de inventario a usar
                         while (true)
                         {
+                            Console.WriteLine("***********************************************************");
                             Console.WriteLine("GESTION DE INVENTARIO EMPRESA: " + nombreEmpresa);
-
+                            Console.WriteLine("***********************************************************");                            // Agregar producto al inventario de productos
+                            // Menu para seleccionar el metodo de gestion de inventario a usar
                             Console.WriteLine("\nAgregar productos al inventario ...");
-                            Console.WriteLine("\nSeleccione un metodo para gestionar el inventario: ");
-                            Console.WriteLine("1.Metodo PEPS");
-                            Console.WriteLine("2.Metodo UEPS");
-                            Console.WriteLine("3.Costo promedio");
-                            Console.WriteLine("4.Volver al menu principal");
-                            Console.Write("\nOpcion: ");
-
-                            while (!int.TryParse(Console.ReadLine(), out opMenu) || opMenu < 1 || opMenu > 4)
+                            Console.WriteLine("1.Agregar productos");
+                            Console.WriteLine("2.Volver al menu principal");
+                            Console.Write("Opcion: ");
+                            // Validacion de opcion ingresada
+                            while (!int.TryParse(Console.ReadLine(), out opMenu) || opMenu < 1 || opMenu > 2)
                             {
                                 Console.WriteLine("Opcion no valida");
+                                Console.Write("Opcion: ");
                             }
 
-
-                            if (opMenu == 4)//ESTE IF SIRVE PARA CONTROLAR EL BUCLE (SALIR)
+                            //Vuelve al menu principal
+                            if (opMenu == 2)
                             {
+                                Console.WriteLine("Volviendo al menu principal ...");
+                                Thread.Sleep(2000);
                                 break;
                             }
 
                             switch (opMenu)
                             {
                                 case 1:
-                                    break;
-
-                                case 2:
-                                    break;
-
-                                case 3:
+                                    Console.WriteLine("\nAgregar producto a inventario");
+                                    Inventario.agregarProductosAInventario();
+                                    Console.WriteLine("Producto agregado ...");
+                                    Console.WriteLine("\nMostrando inventario ...");
+                                    Inventario.mostrarProductos();
+                                    Console.WriteLine();
+                                    Console.ReadKey();
                                     break;
 
                                 default:
+                                    Console.WriteLine("Error desconocido");
                                     break;
                             }
                         }
@@ -100,8 +102,9 @@ namespace Laboratorio_2_Estructura_De_Datos
                         //el user debe poder seleccionar que metodo usar pepe ueps o costo promedio
                         while (true)
                         {
+                            Console.WriteLine("***********************************************************");
                             Console.WriteLine("GESTION DE INVENTARIO EMPRESA: " + nombreEmpresa);
-
+                            Console.WriteLine("***********************************************************");
                             Console.WriteLine("\nVender productos del inventario ...");
                             Console.WriteLine("\nSeleccione un metodo para gestionar el inventario: ");
                             Console.WriteLine("1.Metodo PEPS");
@@ -113,6 +116,7 @@ namespace Laboratorio_2_Estructura_De_Datos
                             while (!int.TryParse(Console.ReadLine(), out opMenu) || opMenu < 1 || opMenu > 4)
                             {
                                 Console.WriteLine("Opcion no valida");
+                                Console.Write("Opcion: ");
                             }
 
 
@@ -139,9 +143,7 @@ namespace Laboratorio_2_Estructura_De_Datos
                         }
                         break;
                     case 3:
-                        //mostrar inventario
-                        mostrarInventario(inventario);
-                        Console.ReadKey();
+                        //Mostrar inventario
                         break;
 
                     default:
@@ -151,23 +153,7 @@ namespace Laboratorio_2_Estructura_De_Datos
             }
         }
 
-        //seccion de metodos utiles para el flujo del programa general
-        static void mostrarInventario(Dictionary<int, List<Producto>> inventario)
-        {
-            //validar si esta vacio
-            if (inventario.Count == 0)
-            {
-                Console.WriteLine("El inventario esta vacio!");
-            }
-            else
-            {
-                foreach (var item in inventario)
-                {
-                    Console.WriteLine(item);
-                }
-            }
-
-
-        }
+        //Seccion de metodos utiles para el flujo del programa general
+       
     }
 }
